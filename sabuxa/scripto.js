ee7bd23011662ed1aa7b02e6c9dba447f356dcd8
BOHMEZIN <137111050+BOHMEZIN@users.noreply.github.com>
2023-09-25 16:48:10 -0400

    Update scripto.js

--- sabuxa/scripto.js
+++ sabuxa/scripto.js
@@ -2,7 +2,7 @@ let musicas = [
     {titulo:'Sweather Weather', artista:'The Neighbourhood', src:'musicas/Sweater Weather.mp3', img:'imagens/sweather.jpg'},
     {titulo:'I miss you', artista:'Blink-182', src:'musicas/I Miss You.mp3', img:'imagens/blink-182.jpg'},
     {titulo:'strangers', artista:'Kenya Grace', src:'musicas/Strangers.mp3', img:'imagens/Strangers.png'},
-    {titulo:'Painkiller', artista:'Judas Priest', src:'musicas/Painkiller.mp3', img:'imagens/painkiller.jpg'},
+    {titulo:'Painkiller', artista:'Judas Priest', src:'musicas/painkiller.mp3', img:'imagens/painkiller.jpg'},
     {titulo:'Assobio Levanta Defunto', artista:'DJ russo', src:'musicas/Assobio Levanta Defunto.mp3', img:'imagens/assobio.jpg'},
     {titulo:'Blade', artista:'Imperanon', src:'musicas/Blade.mp3', img:'imagens/blade.jpg'},
     {titulo:'nanana X cola', artista:'Lewis Hanton', src:'musicas/na na na x Cola.mp3', img:'imagens/nanana.jpg'},
